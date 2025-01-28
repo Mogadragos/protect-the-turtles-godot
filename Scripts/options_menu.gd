5b40f127661f64bab8d6b66b0ea2d100f5481617
ScRalP <quentin.robard@gmail.com>
2025-01-28 18:10:24 +0100

    Button SFX + folder sorting

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -4,6 +4,7 @@ extends Control
 #region Buttons control
 
 func _on_menu_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	SoundFx.button_click()
 
 #endregion
