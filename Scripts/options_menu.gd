2c0ae490b222bfd6c31b2fd9559bb7605ee8fec8
Mogador <jcaillaud17170@gmail.com>
2025-01-29 18:09:56 +0100

    Mute buttons

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -1,10 +1,21 @@
 extends Control
 
+func _ready() -> void:
+	$MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/MarginContainer/MusicButton.set_pressed_no_signal(!SoundFx.music_muted)
+	$MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/MarginContainer2/SfxButton.set_pressed_no_signal(!SoundFx.sfx_muted)
 
 #region Buttons control
 
 func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	SoundFx.button_click()
+
+func _on_music_button_toggled(toggled_on: bool) -> void:
+	SoundFx.toggle_music(toggled_on)
+	SoundFx.button_click()
+
+func _on_sfx_button_toggled(toggled_on: bool) -> void:
+	SoundFx.toggle_sfx(toggled_on)
 	SoundFx.button_click()
 
 #endregion
--- Scripts/sound_fx.gd
+++ Scripts/sound_fx.gd
@@ -1,9 +1,31 @@
 extends Node
 
-var activeVersion: Node;
+var _activeVersion: Node;
+var music_muted = false;
+var sfx_muted = false;
 
 func _ready():
-	activeVersion = $V1;
-
-func button_click():
-	activeVersion.get_node("ButtonClick").play();
+	_activeVersion = $V1;
+	
+func button_click() -> void:
+	_activeVersion.get_node("ButtonClick").play();
+	
+func toggle_music(toggled_on: bool) -> void:
+	music_muted = !toggled_on;
+	_toggle_bus(AudioServer.get_bus_index("Music"), music_muted);
+	
+func toggle_sfx(toggled_on: bool) -> void:
+	sfx_muted = !toggled_on;
+	_toggle_bus(AudioServer.get_bus_index("SFX"), sfx_muted);
+	
+func is_music_active() -> bool:
+	return _is_bus_active(AudioServer.get_bus_index("Music"));
+	
+func is_sfx_active() -> bool:
+	return _is_bus_active(AudioServer.get_bus_index("SFX"));
+	
+func _toggle_bus(bus: int, muted: bool) -> void:
+	AudioServer.set_bus_mute(bus, muted);
+	
+func _is_bus_active(bus: int) -> bool:
+	return AudioServer.get_bus_volume_db(bus) != 0;
